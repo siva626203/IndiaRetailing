cba9cc226e39ed48da5a8122fd4f1ada510ea164
sivakumar <sivakumar.p@riolabz.com>
2024-09-23 09:36:42 +0530

    changes

--- src/app/_components/Footer.tsx
+++ src/app/_components/Footer.tsx
@@ -4,13 +4,13 @@ import I1 from '../../images/image4.png'
 function Footer() {
   return (
     <div className='flex px-11 border-t-2 border-gray-300 mt-4'>
-        <div className="space-y-5">
+        <div className="space-y-5 font-roboto">
             <Image src={I1} alt='icon' width={200} height={66}/>
-            <p className='w-[316px] font-normal text-gray-400 text-[14.5px]'>
-                <span className="font-medium text-gray-400">New Delhi</span><br/> Images Multimedia Ltd.<br/> S-61 A, Pocket S, Okhla Phase II, Okhla Industrial Estate, New Delhi, Delhi 110020 Images Multimedia Ltd.
+            <p className='w-[316px] font-[350] font-roboto text-gray-400 text-[14.5px]'>
+                <span className="font-[700] text-[14.5px] text-[#77808B]">New Delhi</span><br/> Images Multimedia Ltd.<br/> S-61 A, Pocket S, Okhla Phase II, Okhla Industrial Estate, New Delhi, Delhi 110020 Images Multimedia Ltd.
             </p>
-            <p className='w-[316px] font-normal text-gray-400 text-[14.5px]'>
-                <span className="font-medium text-gray-400">Mumbai</span><br/> E 519, Floral Deck Plaza Central MIDC Road, Opp. SEEPZ. Andheri (East) Mumbai 400093
+            <p className='w-[316px] font-[350] font-roboto text-gray-400 text-[14px]'>
+                <span className="font-[700] text-[14.5px] text-[#77808B]">Mumbai</span><br/> E 519, Floral Deck Plaza Central MIDC Road, Opp. SEEPZ. Andheri (East) Mumbai 400093
             </p>
             <p className='w-[316px] font-medium text-gray-400 text-[14.5px]'>
                 <span className='font-semibold text-gray-800'>Contact us</span><br/>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,6 +12,8 @@ import citywalk from '../images/image 34.png'
 import arrow from '../images/down-arrow 1.png'
 import Food from '../images/image 34 (1).png'
 import flower from '../images/image 34 (2).png'
+import Img27 from '../images/image 27.png'
+import Market from '../images/market.png'
 export default function Home() {
   return (
     <div className="">
@@ -121,10 +123,44 @@ export default function Home() {
                   </div>
                 </div>
               </div>
+              
             </div>
             </div>
+            
           </div>
-          
+          <div className="pl-10 space-y-3">
+            <div>
+              <h1 className=" font-roboto font-[500] text-[20.12px] line-[2.26px]">Latest News</h1>
+              <div className="bg-[#CC0000] w-[15.19px] h-[2.26px]  rounded-[5.64px] bg-fixed"></div>
+            </div>
+              <Image alt="imag27" src={Img27} className="w-[343px] h-[184px] rounded-[7.88px]"/>
+              <h1 className="font-inter font-[500] text-[16.94px] line-[22px] ">Unwrapping the Archies’ reinvention plan</h1>
+              <Image src={Market} alt="market" className="w-[342.67px] h-[184px] rounded-[7.88px]"/>
+              <h1 className="font-inter font-[500] text-[16.94px] line-[22px] ">How AI is enhancing stores, How AI is enhancing stores</h1>
+          <div className="flex flex-col gap-5">     
+          <hr className=""/>
+          <div className="">
+         <h1 className="font-inter font-[400] text-[15px] line-[18px] mb-3">Croma opens 58 outlets in 6, Croma retails more than 16,000</h1>
+         <h1 className="font-inter font-[400] text-[13.97px] text-[#666666] line-[17.47px]">These companies created a lot of hype when they listed on the...</h1>
+          </div>
+          <hr className="mt-4"/>
+          <div className="">
+         <h1 className="font-inter font-[400] text-[15px] line-[18px] mb-3">Select Citywalk opens 4 new stores in June</h1>
+         <h1 className="font-inter font-[400] text-[13.97px] text-[#666666] line-[17.47px]">These companies created a lot of hype when they listed on the...</h1>
+          </div>
+          </div>
+          </div>
+       
+        </div>
+        
+        <div className="mt-20 border-t-[1px] border-t-[#AAAAAA] pl-7">
+            <h1 className="font-roboto text-[#121212] text-[20.12px] font-[500] line-[24.14px]">Web Specials</h1>
+            <div className="bg-[#CC0000] w-[15.19px] h-[2.26px]  rounded-[5.64px] bg-fixed"></div>
+             <div className="w-[567.55px] h-[116.91px] absolute mt-[210px] ml-4 space-y-1">
+            <p className="font-inter font-[700] text-[18.28px] line-[24.05px] text-[#FFFFFF]">Rahul Gandhi In Manipur: Chopper Ride After Women Protesters Surround Car</p>
+            <p className="text-[16.85px] font-[400] line-[25.28px] text-[#FFFFFFCC] font-droid-sans">Rahul Gandhi</p>
+            </div>
+            <Image src={Rahul} alt="rahul"  className="rounded-[4.78px] w-[507.59px] h-[251.99px]"/>
         </div>
         <div className="mt-24">
         <Footer/>
